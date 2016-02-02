7e61ccdfa0b89de1fd7d51ab7b8ae079705f4371
Luiz Guilherme <do.guilherme@hotmail.com>
2016-02-02 19:53:40 -0200

    A test Hello World in C

---
+++ helloworld/hello.c
@@ -0,0 +1,16 @@
+/* 
+ * File:   hello.c
+ * Author: luizguilherme5
+ *
+ * Created on 2 de Fevereiro de 2016, 19:51
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+    
+    printf("Hello, world");
+    return (EXIT_SUCCESS);
+}
+
